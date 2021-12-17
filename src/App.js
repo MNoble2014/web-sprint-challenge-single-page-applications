b08ef9a6ab14c06b01a146eecf78c32eeb92bdbe
MNoble2014 <michealsimpson2013@gmail.com>
2021-12-17 11:38:45 -0500

    app.js coded

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
+import { BrowswerRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Form from "./components/Form";
+import Home from "./components/Homepage";
 
-const App = () => {
+const App =() => {
   return (
-    <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+    <div className="header">
+      <nav>
+        <Link to="/"><button id="home-button">Home</button></Link>
+        <Link to="/pizza"><button id="order-pizza">Order Pizza!</button></Link>
+      </nav>
+      <Switch>
+        <Route path="/pizza" component={Form} />
+        <Route path ="/" component={Home} />
+      </Switch>
+    </div>
   );
 };
+
 export default App;
